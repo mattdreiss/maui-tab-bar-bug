131d8ea358674f35b18ab1718266ba5d864c86f9
Matt Dreiss <matt.dreiss@gmail.com>
2024-03-11 10:02:04 -0500

    Example app showing tab bar visibility bug

---
+++ TestAppShell/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace TestAppShell;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ TestAppShell/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace TestAppShell;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
